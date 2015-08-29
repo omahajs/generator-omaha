025f1a47d05d5a8eab51f5ce65298ad367e67cbd
Jason Wohlgemuth <jason.h.wohlgemuth@gmail.com>
2015-08-29 10:10:13 -0500

    Creates folder structure.  Updated README

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,56 +2,79 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var mkdirp = require('mkdirp');
 
 module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
-    var done = this.async();
+    prompting: function () {
+        var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the praiseworthy ' + chalk.red('Techtonic') + ' generator!'
-    ));
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the praiseworthy ' + chalk.red('techtonic') + ' generator!'
+        ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+        var prompts = [{
+            type: 'confirm',
+            name: 'someOption',
+            message: 'Would you like to enable this option?',
+            default: true
+        }];
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
+        this.prompt(prompts, function (props) {
+            this.props = props;
+            // To access props later use this.props.someOption;
 
-      done();
-    }.bind(this));
-  },
-
-  writing: {
-    app: function () {
-      this.fs.copy(
-        this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
-      );
+            done();
+        }.bind(this));
     },
 
-    projectfiles: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
-    }
-  },
+    writing: {
+        app: function () {
+            //workflow
+            mkdirp('config');
+            mkdirp('tasks');
+            //app
+            mkdirp('app/models');
+            mkdirp('app/views');
+            mkdirp('app/controllers');
+            mkdirp('app/modules');
+            mkdirp('app/helpers');
+            mkdirp('app/shims');
+            //assets
+            mkdirp('assets/fonts');
+            mkdirp('assets/images');
+            mkdirp('assets/less');
+            mkdirp('assets/library');
+            mkdirp('assets/templates');
 
-  install: function () {
-    this.installDependencies();
-  }
+            mkdirp('tests');
+            mkdirp('vault');
+            mkdirp('web');
+
+            this.fs.copyTpl(
+                this.templatePath('_package.json'),
+                this.destinationPath('package.json'),
+                {opt: this.props.someOption}
+            );
+            this.fs.copy(
+                this.templatePath('_bower.json'),
+                this.destinationPath('bower.json')
+            );
+        },
+
+        projectfiles: function () {
+            this.fs.copy(
+                this.templatePath('editorconfig'),
+                this.destinationPath('.editorconfig')
+            );
+            this.fs.copy(
+                this.templatePath('jshintrc'),
+                this.destinationPath('.jshintrc')
+            );
+        }
+    },
+
+    install: function () {
+        this.installDependencies();
+    }
 });
