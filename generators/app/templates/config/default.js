4ebc8fd7bd860b1f90ca7084e1bd34ef93e50a35
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2015-11-29 14:40:50 -0600

    Pruned config files, update README and edited index

--- generators/app/templates/config/default.js
+++ generators/app/templates/config/default.js
@@ -41,27 +41,5 @@ module.exports = {
             images:       'images/**/*.{png,jpg,gif,svg}',
             templates:    'templates/**/*.hbs'
         }
-    },
-    execMap: {
-        py: 'python',
-        rb: 'ruby'
-    },
-    session: {
-        secret: uuid.v1(),
-        resave: false,
-        saveUninitialized: false
-    },
-    websocket: {
-        port: 13337
-    },
-    http: {
-        port: process.env.PORT || 3000
-    },
-    log: {
-        level: "error"
-    },
-    csp: {
-        'default-src': '\'self\'',
-        'script-src':  '\'self\' cdnjs.cloudflare.com'
     }
 };
