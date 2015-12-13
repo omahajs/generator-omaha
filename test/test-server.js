8fb4ae6f13a301f1ce462f8acd10f869cc5d9df2
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2015-12-13 17:22:28 -0600

    Integrated json-server into grunt task

--- test/test-server.js
+++ test/test-server.js
@@ -7,26 +7,24 @@ var os = require('os');
 var mkdirp = require('mkdirp');
 
 describe('techtonic:server', function() {
-    describe('when installed into an empty project', function() {
-        before(function(done) {
-            helpers.run(path.join(__dirname, '../generators/server'))
-                .withOptions({skipInstall: true})
-                .on('end', done);
-        });
-        it('creates files', function() {
-            assert.file([
-                'package.json',
-                'app.json',
-                'index.js',
-                'config/default.js',
-                'web/socket.js',
-                'web/server.js',
-                'web/client/index.html'
-            ]);
-        });
-        it('configures files', function() {
-            assert.fileContent('config/default.js', 'port: 13337');
-            assert.fileContent('config/default.js', 'port: process.env.PORT || 8111');
-        });
+    before(function(done) {
+        helpers.run(path.join(__dirname, '../generators/server'))
+            .withOptions({skipInstall: true})
+            .on('end', done);
+    });
+    it('creates files', function() {
+        assert.file([
+            'package.json',
+            'app.json',
+            'index.js',
+            'config/default.js',
+            'web/socket.js',
+            'web/server.js',
+            'web/client/index.html'
+        ]);
+    });
+    it('configures files', function() {
+        assert.fileContent('config/default.js', 'port: 13337');
+        assert.fileContent('config/default.js', 'port: process.env.PORT || 8111');
     });
 });
