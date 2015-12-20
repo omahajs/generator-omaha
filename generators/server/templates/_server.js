8f57b8d5969e92376ad829a4b449fda47d951014
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2015-12-20 10:30:07 -0600

    Added Helmet middleware to harden the server even more

--- generators/server/templates/_server.js
+++ generators/server/templates/_server.js
@@ -3,12 +3,17 @@
  * @see [Using Express.js Middleware]{@link http://expressjs.com/guide/using-middleware.html}
  * @see [Third-Party Middleware]}@link http://expressjs.com/resources/middleware.html}
  * @see [krakenjs/lusca]{@link https://github.com/krakenjs/lusca}
+ * @see [helmetjs/helmet]{@link https://github.com/helmetjs/helmet}
 **/
 var express  = require('express');
 var session  = require('express-session');
 var lusca    = require('lusca');
+var helmet   = require('helmet');
 var compress = require('compression');
 var config   = require('config');
+
+var ninetyDaysInMilliseconds = 7776000000;
+
 var app = express()
     .use(session(config.get('session')))
     .use(function (req, res, next) {
@@ -21,6 +26,13 @@ var app = express()
     .use(lusca.xframe('SAMEORIGIN'))       /** Helps prevent Clickjacking **/
     .use(lusca.hsts({ maxAge: 31536000 }))
     .use(lusca.xssProtection(true))
+    .use(helmet.noSniff())
+    .use(helmet.ieNoOpen())
+    .use(helmet.publicKeyPins({
+      maxAge: ninetyDaysInMilliseconds,
+      sha256s: ['base64==', 'base64=='],   /** Needs to be changed **/
+      includeSubdomains: true
+    }))
     .use(compress())                       /** Use gzip compression **/
     .use(express.static(__dirname));       /** Serve static files **/
 app.get('/', function(req, res) {
