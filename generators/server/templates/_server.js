250746594834bd7c32cea5db41b83490790d7ef8
vagrant <jhwohlgemuth@gmail.com>
2016-01-30 17:50:33 -0600

    Added markdown support for any file in markdown directory

--- generators/server/templates/_server.js
+++ generators/server/templates/_server.js
@@ -5,36 +5,54 @@
  * @see [krakenjs/lusca]{@link https://github.com/krakenjs/lusca}
  * @see [helmetjs/helmet]{@link https://github.com/helmetjs/helmet}
 **/
+var fs       = require('fs');
+var config   = require('config');
+var marked   = require('marked');
 var express  = require('express');
 var session  = require('express-session');
 var lusca    = require('lusca');
 var helmet   = require('helmet');
 var compress = require('compression');
-var config   = require('config');
 
-var ninetyDaysInMilliseconds = 7776000000;
+var NINETY_DAYS_IN_MILLISECONDS = 7776000000;
 
 var app = express()
+    .engine('md', function(path, options, fn) {
+        fs.readFile(path, 'utf8', function(err, str) {
+            if (err) return fn(err);
+            try {
+                var html = marked(str);
+                html = html.replace(/\{([^}]+)\}/g, function(_, name) {
+                    return options[name] || '';
+                })
+                fn(null, html);
+            } catch (err) {
+                fn(err);
+              }
+        });
+    })
+    .set('views', __dirname + '/markdown')
+    .set('view engine', 'md')
     .use(session(config.get('session')))
     .use(function (req, res, next) {
         res.set('X-CSRF', config.get('session').secret);
         return next();
     })
-    .disable('x-powered-by')               /** Do not advertise Express **/
-    .use(lusca.csrf())                     /** Cross Site Request Forgery **/
-    .use(lusca.csp({policy: config.csp}))  /** Content Security Policy **/
-    .use(lusca.xframe('SAMEORIGIN'))       /** Helps prevent Clickjacking **/
+    .disable('x-powered-by')                /** Do not advertise Express **/
+    .use(lusca.csrf())                      /** Cross Site Request Forgery **/
+    .use(lusca.csp({policy: config.csp}))   /** Content Security Policy **/
+    .use(lusca.xframe('SAMEORIGIN'))        /** Helps prevent Clickjacking **/
     .use(lusca.hsts({ maxAge: 31536000 }))
     .use(lusca.xssProtection(true))
     .use(helmet.noSniff())
     .use(helmet.ieNoOpen())
     .use(helmet.publicKeyPins({
-        maxAge: ninetyDaysInMilliseconds,
-        sha256s: ['base64==', 'base64=='],   /** Needs to be changed **/
+        maxAge: NINETY_DAYS_IN_MILLISECONDS,
+        sha256s: ['base64==', 'base64=='],  /** Needs to be changed **/
         includeSubdomains: true
     }))
-    .use(compress())                       /** Use gzip compression **/
-    .use(express.static(__dirname));       /** Serve static files **/
+    .use(compress())                        /** Use gzip compression **/
+    .use(express.static(__dirname));        /** Serve static files **/
 app.get('/', function(req, res) {
     if (res.get('X-CSRF') === config.get('session').secret) {
         res.redirect('/client');
@@ -42,4 +60,8 @@ app.get('/', function(req, res) {
         res.status(412).end();
     }
 });
+app.get('/markdown/:page', function(req, res) {
+    res.render(req.params.page, {title: 'Markdown Example'});
+});
+
 module.exports = app;
