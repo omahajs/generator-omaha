9ca18fef79549178fa09b644606d1c75bfb2fb52
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-05-29 17:12:54 -0500

    Moved command line options into separate file

--- generators/plugin/index.js
+++ generators/plugin/index.js
@@ -1,50 +1,9 @@
 'use strict';
 
-var yeoman    = require('yeoman-generator');
-var mkdirp    = require('mkdirp');
+var yeoman             = require('yeoman-generator');
+var mkdirp             = require('mkdirp');
+var commandLineOptions = require('./commandLineOptions');
 
-var commandLineOptions = {
-    jquery: {
-        type: 'Boolean',
-        desc: 'Add jQuery dependency',
-        defaults: false
-    },
-    underscore: {
-        type: 'Boolean',
-        desc: 'Add Underscore dependency',
-        defaults: false
-    },
-    lodash: {
-        type: 'Boolean',
-        desc: 'Add lodash dependency',
-        defaults: false
-    },
-    ramda: {
-        type: 'Boolean',
-        desc: 'Add Ramda dependency',
-        defaults: false
-    },
-    backbone: {
-        type: 'Boolean',
-        desc: 'Add Backbone dependency (automatically adds Underscore dependency)',
-        defaults: false
-    },
-    marionette: {
-        type: 'Boolean',
-        desc: 'Add Marionette dependency (automatically adds Underscore and Backbone dependencies)',
-        defaults: false
-    },
-    customDependency: {
-        type: 'String',
-        desc: 'Add custom dependency (value provided will be used as global dependency name)',
-        defaults: ''
-    },
-    alias: {
-        type: 'String',
-        desc: 'Set custom variable name to be used in plugin scope for associated custom dependency',
-        defaults: ''
-    }
-};
 var globalNameLookup = {
     root: 'root',
     jquery: '$',
