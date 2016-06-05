90ab984f887c371755f2fb53fd08d031eb1447f4
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-06-05 17:24:21 -0500

    Started working on helper methods for customizing JSON files based on user prompt inputs

--- generators/helpers.js
+++ generators/helpers.js
@@ -1,2 +1,16 @@
+var path   = require('path');
+var fs     = require('fs');
 var _      = require('lodash');
 var extend = require('deep-extend');
+
+function readJSON(fileName) {
+    return JSON.parse(fs.readFileSync(path.join(__dirname, fileName + '.json')).toString());
+}
+function writeJSON(fileName, contents) {
+    fs.writeFileSync(path.join(__dirname, fileName + '.json'), JSON.stringify(contents, null, 4) + '\n');
+}
+function extendJSON(fileName, obj) {
+    writeJSON(fileName, extend(readJSON(fileName), obj));
+}
+
+extendJSON('test', {'boooom': 'boom'});
