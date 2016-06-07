e7f9388ddf05aaea5972d5afbade31b4408e03db
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-06-06 19:22:29 -0500

    Added unit tests and chai dependencies

--- test/test-utils-json.js
+++ test/test-utils-json.js
@@ -1,12 +1,26 @@
-var path  = require('path');
-var fs    = require('fs');
-var utils = require('../generators/utils').json;
+var chai   = require('chai');
+var assert = chai.assert;
+var path   = require('path');
+var fs     = require('fs');
+var utils  = require('../generators/utils').json;
+chai.use(require('chai-shallow-deep-equal'));
 
-function removeJSON(fileName) {fs.unlinkSync(path.join(__dirname, fileName));}
+function removeJSON(fileName) {fs.unlinkSync(fileName);}
 
 describe('Generator JSON utility', function() {
-    it('can create and remove JSON files', function() {
-        utils.write(path.join(__dirname, 'test.json'));
-        removeJSON('test.json');
+    var content;
+    var testJSON = path.join(__dirname, 'test.json');
+    beforeEach(function() {
+        utils.write(testJSON, {foo: 'bar'});
+    });
+    afterEach(function() {
+        removeJSON(testJSON);
+    });
+    it('can extend JSON files', function() {
+        content = utils.read(testJSON);
+        assert.shallowDeepEqual(content, {foo: 'bar'});
+        utils.extend(testJSON, {bar: 'baz'});
+        content = utils.read(testJSON);
+        assert.shallowDeepEqual(content, {foo: 'bar', bar: 'baz'});
     });
 });
