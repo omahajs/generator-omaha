aeee37e1a33301133d778848b334a84ddef97dcb
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-07-02 16:33:16 -0500

    Tweaked run.sh

--- test/run.sh
+++ test/run.sh
@@ -10,21 +10,20 @@ next_step() {
     PASSED=$1
     if echo "$PASSED" | grep -iq "^y" ;then
         npm run demo
-    else
-        rebirth
     fi
     rebirth
     echo "On to the next test..."
 }
 
 mkdir -p $TEST_DIRECTORY
+cd $TEST_DIRECTORY
 
 yo techtonic --defaults --skip-benchmark --skip-coveralls --skip-jsinspect --skip-aria --skip-imagemin
-echo "No benchmark, coveralls, JSInspect, ARIA, imagemin? (y/n)"
+echo "No benchmark, coveralls, JSInspect, ARIA, imagemin? Run demo? (y/n)"
 read passed
 next_step $passed
 
 yo techtonic --defaults --script-bundler browserify --css-preprocessor sass --template-technology underscore
-echo "Browserify, Sass, and Underscore? (y/n)"
+echo "Browserify, Sass, and Underscore? Run demo? (y/n)"
 read passed
 next_step $passed
