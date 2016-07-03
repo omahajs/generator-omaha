ee31e580b962d9bdbe1fb0fce7d928bb2690edba
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-07-02 19:15:59 -0500

    More tweaks to run.sh

--- test/run.sh
+++ test/run.sh
@@ -7,23 +7,26 @@ rebirth() {
     cd $TEST_DIRECTORY
 }
 next_step() {
-    PASSED=$1
-    if echo "$PASSED" | grep -iq "^y" ;then
-        npm run demo
+    read CONTINUE
+    if echo "$CONTINUE" | grep -iq "^y" ;then
+        npm run build
     fi
     rebirth
-    echo "On to the next test..."
 }
 
-mkdir -p $TEST_DIRECTORY
-cd $TEST_DIRECTORY
+if [ -d "$TEST_DIRECTORY" ]; then
+    rebirth
+else
+    mkdir -p $TEST_DIRECTORY
+    cd $TEST_DIRECTORY
+fi
 
+#RLH-_____
 yo techtonic --defaults --skip-benchmark --skip-coveralls --skip-jsinspect --skip-aria --skip-imagemin
-echo "No benchmark, coveralls, JSInspect, ARIA, imagemin? Run demo? (y/n)"
-read passed
-next_step $passed
+echo "NO benchmark, coveralls, JSInspect, ARIA, imagemin? Build code? (y/n)"
+next_step
 
+#BSU-BCJAI
 yo techtonic --defaults --script-bundler browserify --css-preprocessor sass --template-technology underscore
-echo "Browserify, Sass, and Underscore? Run demo? (y/n)"
-read passed
-next_step $passed
+echo "Browserify, Sass, and Underscore? Build code? (y/n)"
+next_step
