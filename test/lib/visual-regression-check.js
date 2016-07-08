23a4b33444f9843583fe4e2ea14c39e0acbf5df0
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-07-07 20:16:32 -0500

    Making real progress with automated visual regression testing

--- test/lib/visual-regression-check.js
+++ test/lib/visual-regression-check.js
@@ -1,22 +1,38 @@
 var path      = require('path');
 var chalk     = require('chalk');
-var Bluebird  = require('bluebird');
+var Promise   = require('bluebird');
 var resemble  = require('node-resemble');
 var Nightmare = require('nightmare');
+var utils     = require('../../generators/app/utils');
 
+var runBuilds;
 var results = {};
-var readFile = Bluebird.promisify(require('fs').readFile);
+var readFile = Promise.promisify(require('fs').readFile);
 readFile(path.join(__dirname, 'builds'), 'utf8').then(function (data) {
+    var i = 0;
     var builds = data.split('\n')
         .map(function(str) {return str.substring(0, 3);})
         .filter(function(str) {return str.length !== 0;});
-    // setInterval(function() {
-    //
-    // }, 6000);
-    builds.slice(1, 2).forEach(function(name, index) {
-        var URL  = 'http://localhost:1235/' + name + '/dist/client/';
-        saveScreenshot(URL, name).then(function() {compare('./test/lib/images/reference.png', './test/lib/images/' + name + '.png', name, results);});
-    });
+    var runBuilds = setInterval(function() {
+        if (i === builds.length) {
+            clearInterval(runBuilds);
+            utils.json.write(path.join(__dirname, 'results.json'), results);
+        } else {
+            var name = builds[i];
+            saveScreenshot('http://localhost:1235/' + name + '/dist/client/', name).then(function() {
+                resemble('./test/lib/images/reference.png').compareTo('./test/lib/images/' + name + '.png').onComplete(function(data) {
+                    var mismatch = parseFloat(data.misMatchPercentage);
+                    var postfix = chalk.green.bold('PASS');
+                    if (mismatch > 0) {
+                        postfix = chalk.red.bold('FAIL ') + chalk.inverse(' ' + mismatch + ' ');
+                    }
+                    console.log(name + ' - ' + postfix);
+                    results[name] = data;
+                    ++i;
+                });
+            });
+        }
+    }, 6000);
 }).catch(function(e) {
     console.log('Error reading file', e);
 });
@@ -28,7 +44,7 @@ function createFilePath(name, ext) {
     return filePath;
 }
 function ScreenshotPlugin(url, name, element) {
-    element = element ? element : 5000;
+    element = element ? element : 3000;
     return function(nightmare) {
         if (url) {
             nightmare.goto(url);
@@ -43,10 +59,4 @@ function saveScreenshot(url, name, element) {
         .viewport(1000, 800)
         .use(ScreenshotPlugin(url, name))
         .end();
-}
-function compare(imgA, imgB, name, results) {
-    resemble(imgA).compareTo(imgB).onComplete(function(data) {
-        console.log(data.misMatchPercentage);
-        results[name] = data;
-    });
 }
