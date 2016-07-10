f850a671ceff2f5a21afb21682e4f53fd945634c
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-07-10 16:35:54 -0500

    Moved stuff around and did some refactoring and clean-up

--- test/lib/visual-regression-check.js
+++ test/lib/visual-regression-check.js
@@ -5,28 +5,30 @@ var resemble  = require('node-resemble');
 var Nightmare = require('nightmare');
 var utils     = require('../../generators/app/utils');
 
+var PORT = '1234';
+
 var runBuilds;
 var results = {};
 var readFile = Promise.promisify(require('fs').readFile);
 readFile(path.join(__dirname, 'builds'), 'utf8').then(function (data) {
     var i = 0;
     var builds = data.split('\n')
-        .map(function(str) {return str.substring(0, 3);})
-        .filter(function(str) {return str.length !== 0;});
+        .map(function getBuildName(str) {return str.split('=')[0];})
+        .filter(function isValidBuildName(str) {return str.length !== 0;});
     var runBuilds = setInterval(function() {
         if (i === builds.length) {
             clearInterval(runBuilds);
             utils.json.write(path.join(__dirname, 'results.json'), results);
         } else {
             var name = builds[i];
-            saveScreenshot('http://localhost:1235/' + name + '/dist/client/', name).then(function() {
-                resemble('./test/lib/images/reference.png').compareTo('./test/lib/images/' + name + '.png').onComplete(function(data) {
+            saveScreenshot('http://localhost:1234/' + name + '/dist/client/', name).then(function() {
+                resemble('./test/lib/reference.png').compareTo('./test/lib/screenshots/' + name + '.png').onComplete(function(data) {
                     var mismatch = parseFloat(data.misMatchPercentage);
                     var postfix = chalk.green.bold('PASS');
                     if (mismatch > 0) {
                         postfix = chalk.red.bold('FAIL ') + chalk.inverse(' ' + mismatch + ' ');
                     }
-                    console.log(name + ' - ' + postfix);
+                    console.log(name + (name.length === 3 ? '      ' : '') + ' - ' + postfix);
                     results[name] = data;
                     ++i;
                 });
@@ -39,7 +41,7 @@ readFile(path.join(__dirname, 'builds'), 'utf8').then(function (data) {
 
 function createFilePath(name, ext) {
     ext = ext ? ext : '.png';
-    var filePath = path.join(__dirname, 'images', name);
+    var filePath = path.join(__dirname, 'screenshots', name);
     filePath += ext;
     return filePath;
 }
