01f4eb92a5ddae573cb4d7b4f8557158930f739b
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-09-10 16:10:28 -0500

    Initial hack at making test suite more autonomous

--- test/lib/generate-build-test.sh
+++ test/lib/generate-build-test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-TEST_DIRECTORY=~/omaha-test-directory
 TEST_LIB_DIRECTORY=$PWD/"$(dirname "$0")"
+TEST_DIRECTORY=$TEST_LIB_DIRECTORY/test-builds-directory
 BUILDS_FILE=$TEST_LIB_DIRECTORY/builds
 BUILDS=$(cut -d'=' -f1 $BUILDS_FILE)
 
