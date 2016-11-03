71bbeeb8340f49cb1f4c41f1cee3b30ab4030d5c
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-11-03 18:05:39 -0500

    Added backbone plugin rules

--- generators/project/templates/config/_eslintrc.js
+++ generators/project/templates/config/_eslintrc.js
@@ -7,5 +7,12 @@ module.exports = {
     plugins: [
         'backbone'
     ],
+    rules: {
+        'backbone/collection-model': ['warn'],
+        'backbone/defaults-on-top': ['warn'],
+        'backbone/model-defaults': ['warn'],
+        'backbone/no-collection-models': ['warn'],
+        'backbone/no-model-attributes': ['warn']
+    },
     extends: 'omaha-prime-grade'
 }
