743c8aa29be2ca0766c43e9a89653f8aed098f62
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2017-07-04 18:07:55 -0500

    Cleared lint errors in example spec

--- generators/project/templates/test/mocha/specs/example.spec.js
+++ generators/project/templates/test/mocha/specs/example.spec.js
@@ -1,18 +1,19 @@
 define(function(require) {
     'use strict';
 
-    require('sinon');
+    const sinon  = require('sinon');
     const expect = require('chai').expect;
     const webapp = require('app');
 
     describe('My Super Cool Web App', () => {
         it('should have a predictable state container', () => {
+            let initialCount = 42;
             expect(webapp.getState().name).to.equal('omaha-project');
-            expect(webapp.getState('count')).to.equal(42);
+            expect(webapp.getState('count')).to.equal(initialCount);// 42
             webapp.dispatch({type: 'INCREMENT'});
-            expect(webapp.getState('count')).to.equal(43);
+            expect(webapp.getState('count')).to.equal(initialCount + 1);// 43
             webapp.dispatch({type: 'DECREMENT'});
-            expect(webapp.getState('count')).to.equal(42);
+            expect(webapp.getState('count')).to.equal(initialCount);// 42
         });
         it('should be able to use SinonJS for servers', () => {
             let server = sinon.fakeServer.create();
@@ -21,11 +22,11 @@ define(function(require) {
         it('should be able to use SinonJS spies', () => {
             let object = {method: () => {}};
             let spy = sinon.spy(object, 'method');
-            spy.withArgs(42);
+            spy.withArgs(2);
             spy.withArgs(1);
-            object.method(42);
+            object.method(2);
             object.method(1);
-            sinon.assert.calledOnce(spy.withArgs(42));
+            sinon.assert.calledOnce(spy.withArgs(2));
             sinon.assert.calledOnce(spy.withArgs(1));
         });
         it('should be able to use SinonJS stubs', () => {
@@ -34,12 +35,12 @@ define(function(require) {
             callback.onSecondCall().returns(2);
             callback.onCall(3).returns(3);
             callback.returns(3);
-            callback.withArgs(42).returns(4);
+            callback.withArgs(2).returns(1);
             expect(callback()).to.equal(1);
             expect(callback()).to.equal(2);
             expect(callback()).to.equal(3);
             expect(callback()).to.equal(3);
-            expect(callback(42)).to.equal(4);
+            expect(callback(2)).to.equal(1);
         });
     });
 });
