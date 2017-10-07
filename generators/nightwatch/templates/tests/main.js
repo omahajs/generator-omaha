3b6146cf589343bebb53453674072c1d90db1636
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2017-10-07 16:26:59 -0500

    Set timeout in setup file and clear some test file lint errors

--- generators/nightwatch/templates/tests/main.js
+++ generators/nightwatch/templates/tests/main.js
@@ -1,23 +1,23 @@
 'use strict';
 
+const ENOUGH_TIME = 5000;
+
 module.exports = {
     disabled: false,
     tags: ['sanity', 'navigation'],
-    'Sanity Check': (browser) => {
+    'Sanity Check': browser => {
         browser
             .url(browser.launch_url)
-            .waitForElementVisible('body', 5000)
+            .waitForElementVisible('body', ENOUGH_TIME)
             .assert.title('Omaha Web App');
     },
-    'Sanity Check (Page Model)': (client) => {
+    'Sanity Check (Page Model)': client => {
         var page = client.page.dev();
         page.navigate()
-            .waitForElementVisible('@main', 5000)
+            .waitForElementVisible('@main', ENOUGH_TIME)
             .log('This is a custom command!')
             .assert.title('Omaha Web App');
         client.end();
     },
-    'End': (browser) => {
-        browser.end();
-    }
+    End: browser => browser.end()
 };
